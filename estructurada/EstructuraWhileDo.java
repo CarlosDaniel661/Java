21efa2fdc2f5f105647162f8f9978b495018804d
CarlosDaniel661 <timonrock2015@gmail.com>
2024-08-19 14:11:30 -0300

    estructurada/EstructuraWhileDo - While

---
+++ estructurada/EstructuraWhileDo.java
@@ -0,0 +1,12 @@
+package estructurada;
+
+public class EstructuraWhileDo {
+    public static void main(String[] args) {
+        
+        int contador = 1;
+        while (contador <= 10) {
+            System.out.println("Hola, soy el número " + contador);
+            contador = contador +1;
+        }
+    }
+}
