bcff59ecfd1cc5e8e7061bed96b4ac35f0c0bc5a
CarlosDaniel661 <timonrock2015@gmail.com>
2024-08-27 15:51:58 -0300

    Escenario 2 Dominio

---
+++ escenario2dominio/Institucion.java
@@ -0,0 +1,19 @@
+package escenario2dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Institucion {
+    private List<Curso> cursos = new ArrayList<>();
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+
+    
+}
